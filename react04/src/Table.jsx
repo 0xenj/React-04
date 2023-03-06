07e638177ae10d8eac76b670480932899f465a10
enzo jakobasch <enzo.jakobasch@gmail.com>
2023-03-06 10:45:03 +0100

    improve step0

--- react04/src/Table.jsx
+++ react04/src/Table.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
 
 function Table(props) {
-    /* Insert your code here */
-}
+  const { entries } = props;
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Price</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {entries.map((entry) => (
+          <tr key={entry.id}>
+            <td>{entry.name}</td>
+            <td>{entry.desc}</td>
+            <td>{entry.price}</td>
+            <button type="button" class="btn-close" aria-label="Close"></button>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default Table;
